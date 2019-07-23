c98f1cc7d4c078ce61dd0c359fe0d79c98708584
Phil Lee <phil.lee.ext@bbc.co.uk>
2019-07-23 18:10:54 +0100

    build the media routes from config values

--- src/app/routes/regex/index.js
+++ src/app/routes/regex/index.js
@@ -1,8 +1,21 @@
 import services from '../../lib/config/services/loadableConfig';
+import { servicesWithRadio, servicesWithTv } from '../config';
 
-const serviceRegex = Object.keys(services).join('|');
+const serviceRegex = `${Object.keys(services).join('|')}`;
+const anyLengthAlphaNumeric = '[a-z0-9]+';
 const idRegex = 'c[a-zA-Z0-9]{10}o';
 const ampRegex = '.amp';
+
+const buildMediaRoutes = servicesWithMedia =>
+  Object.keys(servicesWithMedia)
+    .map(service => {
+      return servicesWithMedia[service]
+        .map(sid => {
+          return `/:service(${service})/${sid}}/:pid(${anyLengthAlphaNumeric})`;
+        })
+        .join('|');
+    })
+    .join('|');
 
 export const articleRegexPath = `/:service(${serviceRegex})/articles/:id(${idRegex}):amp(${ampRegex})?`;
 
@@ -19,3 +32,11 @@ export const frontpageDataRegexPath = `${frontpageRegexPath}.json`;
 export const frontpageManifestRegexPath = `/:service(${serviceRegex})/manifest.json`;
 
 export const frontpageSwRegexPath = `/:service(${serviceRegex})/sw.js`;
+
+export const mediaRadioRegex = `(${buildMediaRoutes(servicesWithRadio)})`;
+
+export const mediaTvRegex = `(${buildMediaRoutes(servicesWithTv)})`;
+
+console.log('mediaRadioRegex', mediaRadioRegex);
+
+console.log('mediaTvRegex', mediaTvRegex);
